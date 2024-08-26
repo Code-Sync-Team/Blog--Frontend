ec1e4abe90a1b81b23451ae453da934c3a73004d
byBlair <jkas8233@naver.com>
2024-08-26 21:46:44 +0900

    로그인 페이지 리액트 구조화

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LoginPage from './Page/LoginPage'
+
+const App = () => {
+  return (
+    <Router>
+      <div className="App">
+        <h1>Welcome to My App</h1>
+        <Routes>
+          <Route path="/login" element={<LoginPage />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
---
+++ src/Page/LoginPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LoginPage = () => {
+    return  (
+        <div className='login-container'>
+            <h2>로그인</h2>
+        </div>
+    );
+};
+
+export default LoginPage;
